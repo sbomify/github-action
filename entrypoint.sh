ea2c2827d0f371881a4449dfbbb6f8696157234d
Viktor Petersson <self@vpetersson.com>
2024-08-21 08:24:27 +0100

    Updates entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,22 @@ if [ ! -f "$SBOM_FILE" ]; then
   exit 1
 fi
 
+# Detect artifact type
+if jq -e '.bomFormat == "CycloneDX"' "$SBOM_FILE" > /dev/null; then
+  FORMAT="cyclonedx"
+elif jq -e '.spdxVersion != null' "$SBOM_FILE" > /dev/null; then
+  FORMAT="spdx"
+else
+  echo "Neither CycloneDX nor SPDX format found in JSON file."
+  exit 1
+fi
+
 # Execute the curl command to upload the SBOM file
 curl -X POST \
   -H "Content-Type: application/json" \
-  -H "Authorization: Token $TOKEN" \
+  -H "Authorization: Bearer $TOKEN" \
   -d @"$SBOM_FILE" \
-  https://app.sbomify.com/api/v1/artifact/sbom
+  "https://app.sbomify.com/api/v1/sboms/artifact/$FORMAT"
 
 # Check the result of the curl command
 if [ $? -ne 0 ]; then
