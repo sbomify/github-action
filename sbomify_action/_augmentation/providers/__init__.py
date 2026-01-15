43c25008f3372a2a2c95c57494a595ce5cb73c1f
Viktor Petersson <self@vpetersson.com>
2026-01-15 13:25:23 +0000

    Fixes linting

--- sbomify_action/_augmentation/providers/__init__.py
+++ sbomify_action/_augmentation/providers/__init__.py
@@ -1,13 +1,12 @@
 """Augmentation providers for fetching organizational metadata."""
 
+# Re-export utility from parent module for backwards compatibility
+from ..utils import is_vcs_augmentation_disabled
 from .bitbucket import BitbucketPipelinesProvider
 from .github import GitHubActionsProvider
 from .gitlab import GitLabCIProvider
 from .json_config import JsonConfigProvider
 from .sbomify_api import SbomifyApiProvider
-
-# Re-export utility from parent module for backwards compatibility
-from ..utils import is_vcs_augmentation_disabled
 
 __all__ = [
     "BitbucketPipelinesProvider",
